d543235445145cb9218594a1f1152d99f8e7807b
Keontez Finch <shone.finch@gmail.com>
2013-08-25 23:09:55 -0400

    Created rails application and added status updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtfulPoets::Application.config.secret_token = '85c85c3df6d229cc32a4861c0648c97cb6bf52ca28dc3e55944eaf1ac2135a5036a97424dffc5ddaf0f57edad9e45bad05e9631017829203553c6b432c886aa6'
